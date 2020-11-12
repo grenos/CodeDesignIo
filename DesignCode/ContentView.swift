1122335b765b40616beef9507b73bc0887706480
vasilis <vasilis.green@gmail.com>
2020-11-12 23:27:17 +0100

    ring view and cards

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -88,7 +88,7 @@ struct ContentView: View {
 			
 			
 			
-			BottomCardView()
+			BottomCardView(show: $showCard)
 				.offset(x: 0, y: showCard ? 360 : 1000)
 				.offset(y: bottomState.height)
 				.blur(radius: show ? 20 : 0)
@@ -205,8 +205,14 @@ struct TitleView: View {
 
 
 
-
+/*
+	Animation on this binding is first controlled by the parrent view (ContentView)
+	Then this view (BottomCardView) binds the parent view binding with the binding of (RingView)
+	and the final animation of the ring is handled by the RingView.
+*/
 struct BottomCardView: View {
+	@Binding var show: Bool
+	
 	var body: some View {
 		VStack(spacing: 20) {
 			Rectangle()
@@ -219,6 +225,23 @@ struct BottomCardView: View {
 				.multilineTextAlignment(.center)
 				.font(.subheadline)
 				.lineSpacing(4)
+			
+			HStack(spacing: 20) {
+				RingView(color1: #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1), color2: #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1), width: 88, height: 88, percent: 78, show: $show)
+				VStack(alignment: .leading, spacing: 8) {
+					Text("SwiftUI").fontWeight(.bold)
+					Text("12 out of 12 sections completed\n10 hours spent so far.")
+						.font(.footnote)
+						.foregroundColor(.gray)
+						.lineSpacing(4)
+				}
+				.padding(20)
+				.background(Color.white)
+				.cornerRadius(20)
+				.shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 10)
+				
+			}
+			
 			Spacer()
 		}
 		.padding(.top, 8)
--- DesignCode/HomeView.swift
+++ DesignCode/HomeView.swift
@@ -40,6 +40,24 @@ struct HomeView: View {
 			.padding(.horizontal)
 			.padding(.leading, 14)
 			.padding(.top, 30)
+			.padding(.bottom, 10)
+			
+			
+			HStack(spacing: 12.0) {
+				RingView(color1: #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1), color2: #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1), width: 44, height: 44, percent: 68, show: .constant(true))
+				VStack(alignment: .leading, spacing: 4) {
+					Text("6 minutes left")
+						.font(.subheadline)
+						.fontWeight(.bold)
+					Text("Watched 10 minutes today")
+						.font(.caption)
+				}
+			}
+			.padding(8)
+			.background(Color.white)
+			.cornerRadius(20)
+			.shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
+			.shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 1)
 			
 			
 			ScrollView(.horizontal, showsIndicators: false) {
